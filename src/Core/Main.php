122314e68f3a69681a5f1cd2d8d0b5a81607ca09
Njengah <joe@njengah.com>
2025-01-03 21:06:12 +0300

    code clean up

--- src/Core/Main.php
+++ src/Core/Main.php
@@ -25,21 +25,11 @@ class Main {
 
     private function load_dependencies() {
         $this->loader = new Loader();
-
-        
-        $this->loader->add_action( 'admin_init', $this, 'admin_init' );
-        $this->loader->add_action( 'wp_footer', $this, 'display_footer_message' );
     }
 
     private function register_hooks() {
         $this->loader->run();
     }
 
-    public function admin_init() {
-       
-    }
-
-    public function display_footer_message() {
-        echo '<p>This is a footer message added by the WP Category Media plugin.</p>';
-    }
+   
 }
